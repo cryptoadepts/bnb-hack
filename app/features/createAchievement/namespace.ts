17cd6b44392b9d654df9e3bd6e2a3de5d6250a2a
elshan.eth <elshan.eth>
2022-09-11 16:10:59 +0400

    fix hook

--- app/features/createAchievement/namespace.ts
+++ app/features/createAchievement/namespace.ts
@@ -9,25 +9,31 @@ export const images = [
   {
     id: "1",
     src: image1,
+    hash: "Qmd9Ei9Vq8TREsUsxLQUyhuRaDpTufixmufCoKiFQKptBZ",
   },
   {
     id: "2",
     src: image2,
+    hash: "QmNu1xE2N3cFhxp7WhZsc8SEYhYAkHcsr7ycK26JVMxR3K",
   },
   {
     id: "3",
     src: image3,
+    hash: "QmYJ74xggDGiQCaU6fRJg1ejfPjmU4R38YTzvdyofqZTxU",
   },
   {
     id: "4",
     src: image4,
+    hash: "QmfJ4FxXQTMNobmxryQPPa6REL2aDz366QHVGp537Qwqa2",
   },
   {
     id: "5",
     src: image5,
+    hash: "QmNZ1T8KokEywySJKneRt8kCKKbpj15CXHzvZdBParMZ9K",
   },
   {
     id: "6",
     src: image6,
+    hash: "QmQvK48PYxc8Si8xicFEgLokfNZ5PJEC55k4Ro8jy5iXnB",
   },
 ];
